1f214c7f450b892424893e5ed83117d66a628318
fletcher <luofletcher@outlook.com>
2024-04-02 19:53:07 +0800

    complete

--- src/main/java/com/example/TliasApplication.java
+++ src/main/java/com/example/TliasApplication.java
@@ -1,10 +1,7 @@
 package com.example;
 
-import com.example.config.MyImportSelector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Import;
 
 //@Import({MyImportSelector.class})
 //@ServletComponentScan
